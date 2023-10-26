8274d50d6ed761378d3c39de757ecffd39e263aa
Josh Mondy <55757183+mondyjosh@users.noreply.github.com>
2023-10-26 16:05:30 -0500

    Added wiki doc step to shell script.

---
+++ create-symlinks.sh
@@ -0,0 +1,10 @@
+# There are better and less manual ways to do this
+# TODO: Investigate install scripts and boostrapping tooling (i.e., Ansible)
+
+ln -s ~/.config/nvim ~/.dotfiles/nvim    
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh/.zsh_profile ~/.zsh_profile
+ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/bin/.local/scripts/* ~/.local/bin
