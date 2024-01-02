fcd9d7864f91fbc41d97f6e164340ff2a30dc5c7
Josh Mondy <josh@mondyjosh-ubuntu.attlocal.net>
2024-01-02 13:42:27 -0600

    Added an alacritty file while I play with alacritty; moved tmux and gitconfig to .config folders.

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,14 +1,28 @@
 # There are better and less manual ways to do this
 # TODO: Investigate install scripts and boostrapping tooling (i.e., Ansible)
 
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/zsh/.zsh_profile ~/.zsh_profile
-ln -s ~/.dotfiles/zsh/.spaceshiprc.zsh ~/.spaceshiprc.zsh
-ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+# Enforcing XDG Base Directory Specification
+DOTFILE_DIR=$HOME/.dotfiles
+CONFIG_DIR=$HOME/.config
+SCRIPT_DIR=$HOME/.local/bin
 
-# Create and symlink scripts folder 
-mkdir -p ~/.local/bin
-ln -s ~/.dotfiles/scripts/* ~/.local/bin
+mkdir -p $CONFIG_DIR/alacritty
+ln -sf $DOTFILE_DIR/alacritty/alacritty.toml $CONFIG_DIR/alacritty/alacritty.toml
 
+mkdir -p $CONFIG_DIR/git
+ln -sf $DOTFILE_DIR/git/.gitconfig $CONFIG_DIR/git/.gitconfig
 
+# TODO: Make tmux source from $XDG_CONFIG_HOME
+mkdir -p $CONFIG_DIR/tmux
+ln -sf $DOTFILE_DIR/tmux/tmux.conf $CONFIG_DIR/tmux/tmux.conf
+
+# TODO: Make oh-my-zsh source from $XDG_CONFIG_HOME
+# https://gist.github.com/fredjoseph/e81be37b8605590ef7f4cfaef1f476d2
+mkdir -p $CONFIG_DIR/zsh
+ln -sf $DOTFILE_DIR/zsh/.zshrc $HOME/.zshrc
+ln -sf $DOTFILE_DIR/zsh/.zsh_profile $HOME/.zsh_profile
+ln -sf $DOTFILE_DIR/zsh/.spaceshiprc.zsh $HOME/.spaceshiprc.zsh
+
+# TODO: Reconsider - Maybe this isn't the best place to put my scripts
+mkdir -p $SCRIPT_DIR
+ln -sf $DOTFILE_DIR/scripts/* ~/.local/bin
